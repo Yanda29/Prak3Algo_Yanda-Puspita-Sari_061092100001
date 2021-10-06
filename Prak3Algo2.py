48df7c3a1e6e6ef63415c9b984843e88e29bf4e7
Yanda29 <91973633+Yanda29@users.noreply.github.com>
2021-10-06 23:46:54 +0700

    Add files via upload

---
+++ Prak3Algo2.py
@@ -0,0 +1,11 @@
+print("@   @  @@@@@  @    @  @@@@     @@@@@")
+print(" @ @   @   @  @ @  @  @    @   @   @")
+print("  @    @@@@@  @  @ @  @     @  @@@@@")
+print("  @    @   @  @   @@  @    @   @   @")
+print("  @    @   @  @    @  @@@@     @   @")
+a=int(input("masukan angka pertama = "))
+b=int(input("masukan angka terakhir = "))
+while a<b:
+  print(a, b)
+  a = a+1
+  b = b-1
